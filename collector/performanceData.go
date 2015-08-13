73239fb3efec8a0b4b480a7470e791d52eafb1d9
Griesbacher <philip@griesbacher.org>
2015-08-13 08:50:16 +0200

    reorganized

--- collector/performanceData.go
+++ collector/performanceData.go
@@ -1,1 +1,24 @@
 package collector
+import "fmt"
+
+type PerformanceData struct {
+	hostname         string
+	service          string
+	command          string
+	performanceLabel string
+	performanceType  string
+	unit             string
+	time             string
+	value            string
+	fieldseperator   string
+}
+
+func (p PerformanceData) String() string {
+	return fmt.Sprintf(`%s%s%s%s%s%s%s%s%s value=%s %s`,
+		p.hostname, p.fieldseperator,
+		p.service, p.fieldseperator,
+		p.command, p.fieldseperator,
+		p.performanceLabel, p.fieldseperator,
+		p.performanceType,
+		p.value, p.time)
+}
