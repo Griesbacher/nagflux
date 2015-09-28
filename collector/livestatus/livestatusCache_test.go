23fadb9166614bf760d1b9f1fb732018a16ce72e
Philip Griesbacher <philip@griesbacher.org>
2015-09-28 10:59:24 +0200

    livestatuscache test added

---
+++ collector/livestatus/livestatusCache_test.go
@@ -0,0 +1,20 @@
+package livestatus
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestAddDowntime(t *testing.T) {
+	cache := LivestatusCache{make(map[string]map[string]string)}
+	if !reflect.DeepEqual(cache.downtime, make(map[string]map[string]string)) {
+		t.Error("Cache should be empty at the beginning.")
+	}
+	cache.addDowntime("hostname", "servicename" ,"123")
+	intern := map[string]map[string]string{"hostname":map[string]string{"servicename":"123"}}
+	if !reflect.DeepEqual(cache.downtime, intern) {
+		t.Error("Cache should be empty at the beginning.")
+	}
+}
+
+
