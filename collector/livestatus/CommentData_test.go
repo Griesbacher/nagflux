5a4134b0d88c7681a3fc385ad58d14e1ec07230e
Philip Griesbacher <philip@griesbacher.org>
2015-11-25 12:58:59 +0100

    formated

--- collector/livestatus/CommentData_test.go
+++ collector/livestatus/CommentData_test.go
@@ -8,15 +8,15 @@ var PrintCommentData = []struct {
 	input  CommentData
 	output string
 }{
-	{CommentData{Data: Data{fieldSeperator: "&", hostName: "host 1", serviceDisplayName: "service 1", author: "philip",comment:"hallo world"}, entryType: "1"},
+	{CommentData{Data: Data{fieldSeperator: "&", hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world"}, entryType: "1"},
 		`host\ 1&service\ 1&messages,type=comment,author=philip value="hallo world" 000`},
-	{CommentData{Data: Data{fieldSeperator: "&", hostName: "host 1", serviceDisplayName: "service 1", author: "philip",comment:"hallo world"}, entryType: "2"},
+	{CommentData{Data: Data{fieldSeperator: "&", hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world"}, entryType: "2"},
 		`host\ 1&service\ 1&messages,type=downtime,author=philip value="hallo world" 000`},
-	{CommentData{Data: Data{fieldSeperator: "&", hostName: "host 1", serviceDisplayName: "service 1", author: "philip",comment:"hallo world"}, entryType: "3"},
+	{CommentData{Data: Data{fieldSeperator: "&", hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world"}, entryType: "3"},
 		`host\ 1&service\ 1&messages,type=flapping,author=philip value="hallo world" 000`},
-	{CommentData{Data: Data{fieldSeperator: "&", hostName: "host 1", serviceDisplayName: "service 1", author: "philip",comment:"hallo world"}, entryType: "4"},
+	{CommentData{Data: Data{fieldSeperator: "&", hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world"}, entryType: "4"},
 		`host\ 1&service\ 1&messages,type=acknowledgement,author=philip value="hallo world" 000`},
-	{CommentData{Data: Data{fieldSeperator: "&", hostName: "host 1", serviceDisplayName: "service 1", author: "philip",comment:"hallo world"}, entryType: "5"},
+	{CommentData{Data: Data{fieldSeperator: "&", hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world"}, entryType: "5"},
 		`host\ 1&service\ 1&messages,author=philip value="hallo world" 000`},
 }
 
@@ -31,13 +31,13 @@ func TestSanitizeValuesComment(t *testing.T) {
 
 func TestPrintComment(t *testing.T) {
 	t.Parallel()
-	comment := CommentData{Data: Data{fieldSeperator: "&", hostName: "host 1", serviceDisplayName: "service 1", author: "philip",comment:"hallo world"}, entryType: "1"}
+	comment := CommentData{Data: Data{fieldSeperator: "&", hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world"}, entryType: "1"}
 	if !didThisPanic(comment.Print, 0.8) {
 		t.Errorf("This should panic, due to unsuported influxdb version")
 	}
-	for _, data := range PrintCommentData{
+	for _, data := range PrintCommentData {
 		actual := data.input.Print(0.9)
-		if actual != data.output{
+		if actual != data.output {
 			t.Errorf("Print(%s): expected: %s, actual: %s", data.input, data.output, actual)
 		}
 	}
