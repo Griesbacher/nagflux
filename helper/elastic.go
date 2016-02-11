76687563843c904b2cd56598b214c8254bd3f18c
Philip Griesbacher <philip@griesbacher.org>
2016-02-11 11:11:10 +0100

    functions to handle elasicsearch specific tasks

--- helper/elastic.go
+++ helper/elastic.go
@@ -1,17 +1,31 @@
 package helper
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 //CreateJSONFromStringMap creates a part of a JSON object
 func CreateJSONFromStringMap(input map[string]string) string {
 	result := ""
 	for k, v := range input {
-		result += fmt.Sprintf(`,"%s":`, k)
-		if IsStringANumber(v) {
-			result += v
-		} else {
-			result += fmt.Sprintf(`"%s"`, v)
-		}
+		result += fmt.Sprintf(`,"%s":%s`, k, GenJSONValueString(v))
 	}
 	return result
+}
+
+//GenJSONValueString quotes the string if it's not a number.
+func GenJSONValueString(input string) string {
+	if IsStringANumber(input) {
+		return input
+	}
+	return fmt.Sprintf(`"%s"`, input)
+}
+
+//SanitizeElasicInput escapes backslashes and trims single ticks.
+func SanitizeElasicInput(input string) string {
+	input = strings.Trim(input, `'`)
+	input = strings.Replace(input, `\`, `\\`, -1)
+	input = strings.Replace(input, `"`, `\"`, -1)
+	return input
 }
