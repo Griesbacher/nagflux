c2b9276e83c1104c2bdc6bd31755a86a417a3101
Philip Griesbacher <philip@griesbacher.org>
2016-02-11 14:45:36 +0100

    format

--- helper/influx_test.go
+++ helper/influx_test.go
@@ -42,7 +42,7 @@ func TestSanitizeMap(t *testing.T) {
 	t.Parallel()
 	for _, data := range SanitizeInfluxDataMap {
 		actual := SanitizeMap(data.input)
-		if !reflect.DeepEqual(actual , data.output ){
+		if !reflect.DeepEqual(actual, data.output) {
 			t.Errorf("SanitizeInfluxData(%s): expected: %s, actual: %s", data.input, data.output, actual)
 		}
 	}
