b28a0d4d35cea0debc07a8e9a43fa6016d44477d
Philip Griesbacher <philip@griesbacher.org>
2016-02-15 13:30:32 +0100

    fixed flapping test

--- helper/elastic_test.go
+++ helper/elastic_test.go
@@ -3,20 +3,21 @@ package helper
 import "testing"
 
 var CreateJSONFromStringMapData = []struct {
-	input    map[string]string
-	expected string
+	input     map[string]string
+	expected  string
+	alternate string
 }{
-	{map[string]string{"a": "1"}, `,"a":1`},
-	{map[string]string{"a": "b"}, `,"a":"b"`},
-	{map[string]string{"a": "1", "2": "b"}, `,"a":1,2:"b"`},
+	{map[string]string{"a": "1"}, `,"a":1`, ""},
+	{map[string]string{"a": "b"}, `,"a":"b"`, ""},
+	{map[string]string{"a": "1", "2": "b"}, `,"a":1,2:"b"`, `,2:"b","a":1`},
 }
 
 func TestCreateJSONFromStringMap(t *testing.T) {
 	t.Parallel()
 	for _, data := range CreateJSONFromStringMapData {
 		actual := CreateJSONFromStringMap(data.input)
-		if actual != data.expected {
-			t.Errorf("CreateJSONFromStringMap(%s): expected:%s, actual:%s", data.input, data.expected, actual)
+		if !(actual == data.expected || actual == data.alternate) {
+			t.Errorf("CreateJSONFromStringMap(%s): expected:%s or %s, actual:%s", data.input, data.expected, data.alternate, actual)
 		}
 	}
 }
