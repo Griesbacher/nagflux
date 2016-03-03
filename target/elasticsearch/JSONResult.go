a2de0e4b56e2ba2719c0a766e0c36f8538770d66
Philip Griesbacher <philip@griesbacher.org>
2016-03-03 12:39:24 +0100

    changed json object

--- target/elasticsearch/JSONResult.go
+++ target/elasticsearch/JSONResult.go
@@ -5,10 +5,10 @@ type JSONResult struct {
 	Errors bool `json:"errors"`
 	Items  []struct {
 		Create struct {
-			_id    string `json:"_id"`
-			_index string `json:"_index"`
-			_type  string `json:"_type"`
-			Error  struct {
+			ID    string `json:"_id"`
+			Index string `json:"_index"`
+			Type  string `json:"_type"`
+			Error struct {
 				CausedBy struct {
 					Reason string `json:"reason"`
 					Type   string `json:"type"`
@@ -16,13 +16,13 @@ type JSONResult struct {
 				Reason string `json:"reason"`
 				Type   string `json:"type"`
 			} `json:"error"`
-			_shards struct {
+			Shards struct {
 				Failed     int `json:"failed"`
 				Successful int `json:"successful"`
 				Total      int `json:"total"`
 			} `json:"_shards"`
-			_version int `json:"_version"`
-			Status   int `json:"status"`
+			Version int `json:"_version"`
+			Status  int `json:"status"`
 		} `json:"create"`
 	} `json:"items"`
 	Took int `json:"took"`
