5f3c9854fe5a02ca00992eda8a8041f040711b2a
Philip Griesbacher <philip@griesbacher.org>
2016-03-26 12:41:04 +0100

    added tests

--- collector/livestatus/CommentData_test.go
+++ collector/livestatus/CommentData_test.go
@@ -2,22 +2,41 @@ package livestatus
 
 import (
 	"testing"
+	"github.com/griesbacher/nagflux/config"
+	"fmt"
+	"github.com/griesbacher/nagflux/logging"
 )
 
 var PrintCommentData = []struct {
-	input  CommentData
-	output string
+	input        CommentData
+	outputInflux string
+	outputElastic string
 }{
-	{CommentData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world"}, entryType: "1"},
-		`messages,host=host\ 1,service=service\ 1,type=comment,author=philip message="hallo world" 000`},
-	{CommentData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world"}, entryType: "2"},
-		`messages,host=host\ 1,service=service\ 1,type=downtime,author=philip message="hallo world" 000`},
-	{CommentData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world"}, entryType: "3"},
-		`messages,host=host\ 1,service=service\ 1,type=flapping,author=philip message="hallo world" 000`},
-	{CommentData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world"}, entryType: "4"},
-		`messages,host=host\ 1,service=service\ 1,type=acknowledgement,author=philip message="hallo world" 000`},
-	{CommentData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world"}, entryType: "5"},
-		`messages,host=host\ 1,service=service\ 1,author=philip message="hallo world" 000`},
+	{CommentData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world", entryTime:"1458988932000"}, entryType: "1"},
+		`messages,host=host\ 1,service=service\ 1,type=comment,author=philip message="hallo world" 1458988932000000`,
+		`{"index":{"_index":"index-2016.03","_type":"messages"}}
+{"timestamp":1458988932000000,"message":"hallo world","author":"philip","host":"host 1","service":"service 1","type":"comment"}
+`},
+	{CommentData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world", entryTime:"1458988932000"}, entryType: "2"},
+		`messages,host=host\ 1,service=service\ 1,type=downtime,author=philip message="hallo world" 1458988932000000`,
+		`{"index":{"_index":"index-2016.03","_type":"messages"}}
+{"timestamp":1458988932000000,"message":"hallo world","author":"philip","host":"host 1","service":"service 1","type":"downtime"}
+`},
+	{CommentData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world", entryTime:"1458988932000"}, entryType: "3"},
+		`messages,host=host\ 1,service=service\ 1,type=flapping,author=philip message="hallo world" 1458988932000000`,
+		`{"index":{"_index":"index-2016.03","_type":"messages"}}
+{"timestamp":1458988932000000,"message":"hallo world","author":"philip","host":"host 1","service":"service 1","type":"flapping"}
+`},
+	{CommentData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world", entryTime:"1458988932000"}, entryType: "4"},
+		`messages,host=host\ 1,service=service\ 1,type=acknowledgement,author=philip message="hallo world" 1458988932000000`,
+		`{"index":{"_index":"index-2016.03","_type":"messages"}}
+{"timestamp":1458988932000000,"message":"hallo world","author":"philip","host":"host 1","service":"service 1","type":"acknowledgement"}
+`},
+	{CommentData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world", entryTime:"1458988932000"}, entryType: "5"},
+		`messages,host=host\ 1,service=service\ 1,author=philip message="hallo world" 1458988932000000`,
+		`{"index":{"_index":"index-2016.03","_type":"messages"}}
+{"timestamp":1458988932000000,"message":"hallo world","author":"philip","host":"host 1","service":"service 1","type":""}
+`},
 }
 
 func TestSanitizeValuesComment(t *testing.T) {
@@ -29,16 +48,32 @@ func TestSanitizeValuesComment(t *testing.T) {
 	}
 }
 
-func TestPrintComment(t *testing.T) {
+func TestPrintInfluxdbComment(t *testing.T) {
 	t.Parallel()
+	logging.InitTestLogger()
 	comment := CommentData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world"}, entryType: "1"}
 	if !didThisPanic(comment.PrintForInfluxDB, "0.8") {
 		t.Errorf("This should panic, due to unsuported influxdb version")
 	}
 	for _, data := range PrintCommentData {
 		actual := data.input.PrintForInfluxDB("0.9")
-		if actual != data.output {
-			t.Errorf("Print(%s): expected: %s, actual: %s", data.input, data.output, actual)
+		if actual != data.outputInflux {
+			t.Errorf("Print(%s): expected: %s, actual: %s", data.input, data.outputInflux, actual)
+		}
+	}
+}
+
+func TestPrintElasticsearchComment(t *testing.T) {
+	logging.InitTestLogger()
+	config.InitConfigFromString(fmt.Sprintf(Config, "monthly"))
+	comment := CommentData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip", comment: "hallo world", entryTime:"1458988932000"}, entryType: "1"}
+	if !didThatPanic(comment.PrintForElasticsearch, "1.0", "index") {
+		t.Errorf("This should panic, due to unsuported elasticsearch version")
+	}
+	for _, data := range PrintCommentData {
+		actual := data.input.PrintForElasticsearch("2.0", "index")
+		if actual != data.outputElastic {
+			t.Errorf("Print(%s): expected: %s, actual: %s", data.input, data.outputElastic, actual)
 		}
 	}
 }
--- collector/livestatus/DowntimeData_test.go
+++ collector/livestatus/DowntimeData_test.go
@@ -3,6 +3,8 @@ package livestatus
 import (
 	"github.com/griesbacher/nagflux/logging"
 	"testing"
+	"github.com/griesbacher/nagflux/config"
+	"fmt"
 )
 
 func TestSanitizeValuesDowntime(t *testing.T) {
@@ -14,8 +16,7 @@ func TestSanitizeValuesDowntime(t *testing.T) {
 	}
 }
 
-func TestPrintDowntime(t *testing.T) {
-	t.Parallel()
+func TestPrintInfluxdbDowntime(t *testing.T) {
 	logging.InitTestLogger()
 	down := DowntimeData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip"}, endTime: "123"}
 	if !didThisPanic(down.PrintForInfluxDB, "0.8") {
@@ -27,5 +28,25 @@ func TestPrintDowntime(t *testing.T) {
 messages,host=host\ 1,service=service\ 1,type=downtime,author=philip value="Downtime end: <br>" 123000`
 	if result != expected {
 		t.Errorf("The result did not match the expected. Result: %s Expected %s", result, expected)
+	}
+}
+
+func TestPrintElasticsearchDowntime(t *testing.T) {
+	logging.InitTestLogger()
+	config.InitConfigFromString(fmt.Sprintf(Config, "monthly"))
+	down := DowntimeData{Data: Data{hostName: "host 1", serviceDisplayName: "service 1", author: "philip", entryTime:"1458988932000"}, endTime: "123"}
+	if !didThatPanic(down.PrintForElasticsearch, "1.0", "index") {
+		t.Errorf("This should panic, due to unsuported elasticsearch version")
+	}
+
+	result := down.PrintForElasticsearch("2.0", "index")
+	expected := `{"index":{"_index":"index-2016.03","_type":"messages"}}
+{"timestamp":1458988932000000,"message":"Downtime start: <br>","author":"philip","host":"host 1","service":"service 1","type":"downtime"}
+
+{"index":{"_index":"index-1970.01","_type":"messages"}}
+{"timestamp":123000,"message":"Downtime end: <br>","author":"philip","host":"host 1","service":"service 1","type":"downtime"}
+`
+	if result != expected {
+		t.Errorf("The result did not match the expected. Result: %sExpected: %s", result, expected)
 	}
 }
