9037aa509e42973a807494fcdc7c29f58eca6a32
Philip Griesbacher <philip@griesbacher.org>
2016-09-20 10:17:41 +0200

    changed circle config

--- coverage.sh
+++ coverage.sh
@@ -2,12 +2,16 @@
 
 function coverage {
 	echo $1":"
-	go test -v -covermode=count -coverprofile=cover.tmp $1
+	fail=`go test -v -covermode=count -coverprofile=cover.tmp $1`
 	if [ -f cover.tmp ]
     then
         cat cover.tmp | tail -n +2 >> cover.out
         rm cover.tmp
     fi
+	if [[ $fail == *FAIL* ]]; then
+		echo $fail
+		exit 1
+	fi
 }
 
 PackageRoot='github.com/griesbacher/nagflux/'
@@ -15,9 +19,14 @@ PackageRoot='github.com/griesbacher/nagflux/'
 echo "mode: count" > cover.out
 for dir in $(find `ls` -type d);
 do
+if [[ "$dir" == vendor* ]]; then
+	continue
+fi
+
 if ls $dir/*_test.go &> /dev/null; then
 	coverage $PackageRoot$dir
 fi
 done
 
-#coverage $PackageRoot
+go test -v $PackageRoot
+exit $?
