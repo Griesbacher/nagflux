d038e58f192a81b108797f8f4791ec5ea8ebb47c
Philip Griesbacher <philip@griesbacher.org>
2017-03-16 14:33:41 +0100

    formated

--- collector/Filterable_test.go
+++ collector/Filterable_test.go
@@ -9,23 +9,23 @@ var testTargetFilterData = []struct {
 	targetToTest string
 	expected     bool
 }{
-	{Filterable{Filter:All}, All, true },
-	{Filterable{Filter:All}, "foo", true },
-	{Filterable{Filter:"foo"}, All, true },
-	{Filterable{Filter:"foo"}, "foo", true },
-	{Filterable{Filter:"bar"}, "foo", false },
-	{Filterable{Filter:"foo"}, "bar", false },
-	{Filterable{Filter:""}, "", true },
-	{Filterable{Filter:""}, "foo", false },
-	{Filterable{Filter:"foo"}, "", false },
-	{Filterable{Filter:"foo,bar"}, "", false },
-	{Filterable{Filter:"foo,bar"}, "foo", true },
-	{Filterable{Filter:"foo,bar"}, "bar", true },
-	{Filterable{Filter:"foo,bar"}, "bar,foo", true },
-	{Filterable{Filter:""}, "foo,bar", false },
-	{Filterable{Filter:"foo"}, "foo,bar", true },
-	{Filterable{Filter:"bar"}, "foo,bar", true },
-	{Filterable{Filter:"bar,foo"}, "foo,bar", true },
+	{Filterable{Filter: All}, All, true},
+	{Filterable{Filter: All}, "foo", true},
+	{Filterable{Filter: "foo"}, All, true},
+	{Filterable{Filter: "foo"}, "foo", true},
+	{Filterable{Filter: "bar"}, "foo", false},
+	{Filterable{Filter: "foo"}, "bar", false},
+	{Filterable{Filter: ""}, "", true},
+	{Filterable{Filter: ""}, "foo", false},
+	{Filterable{Filter: "foo"}, "", false},
+	{Filterable{Filter: "foo,bar"}, "", false},
+	{Filterable{Filter: "foo,bar"}, "foo", true},
+	{Filterable{Filter: "foo,bar"}, "bar", true},
+	{Filterable{Filter: "foo,bar"}, "bar,foo", true},
+	{Filterable{Filter: ""}, "foo,bar", false},
+	{Filterable{Filter: "foo"}, "foo,bar", true},
+	{Filterable{Filter: "bar"}, "foo,bar", true},
+	{Filterable{Filter: "bar,foo"}, "foo,bar", true},
 }
 
 func TestFilterable_TestTargetFilter(t *testing.T) {
@@ -38,7 +38,7 @@ func TestFilterable_TestTargetFilter(t *testing.T) {
 
 func TestFilterable_TestTargetFilterObj(t *testing.T) {
 	for i, data := range testTargetFilterData {
-		if data.perfFilter.TestTargetFilterObj(Filterable{Filter:data.targetToTest}) != data.expected {
+		if data.perfFilter.TestTargetFilterObj(Filterable{Filter: data.targetToTest}) != data.expected {
 			t.Errorf("%d went wrong. Filterable: %s, ToTest: %s", i, data.perfFilter, data.targetToTest)
 		}
 	}
