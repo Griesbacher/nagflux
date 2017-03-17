19a63bf966298af508d0289cf2ab8d1f50df03e6
Philip Griesbacher <philip@griesbacher.org>
2017-03-17 10:24:14 +0100

    updated livestatus test

--- collector/livestatus/Collector_test.go
+++ collector/livestatus/Collector_test.go
@@ -9,10 +9,19 @@ import (
 )
 
 func TestNewLivestatusCollector(t *testing.T) {
-	livestatus := &MockLivestatus{"localhost:6559", "tcp", map[string]string{}, true}
+	livestatus := &MockLivestatus{
+		LivestatusAddress:"localhost:6559",
+		ConnectionType:"tcp",
+		Queries:map[string]string{},
+		isRunning: true,
+	}
 	go livestatus.StartMockLivestatus()
-	connector := &Connector{logging.GetLogger(), "localhost:6559", "tcp"}
-	collector := NewLivestatusCollector(make(collector.ResultQueues), connector, false)
+	connector := &Connector{
+		Log:logging.GetLogger(),
+		LivestatusAddress:"localhost:6559",
+		ConnectionType:"tcp",
+	}
+	collector := NewLivestatusCollector(make(collector.ResultQueues), connector, "")
 	if collector == nil {
 		t.Error("Constructor returned null pointer")
 	}
@@ -20,7 +29,7 @@ func TestNewLivestatusCollector(t *testing.T) {
 }
 
 func TestAddTimestampToLivestatusQuery(t *testing.T) {
-	if addTimestampToLivestatusQuery(QueryIcinga2ForNotifications) != fmt.Sprintf(QueryIcinga2ForNotifications, time.Now().Add(intervalToCheckLivestatus/100*-150).Unix()) {
+	if addTimestampToLivestatusQuery(QueryIcinga2ForNotifications) != fmt.Sprintf(QueryIcinga2ForNotifications, time.Now().Add(intervalToCheckLivestatus / 100 * -150).Unix()) {
 		t.Error("addTimestampToLivestatusQuery has changed")
 	}
 }
