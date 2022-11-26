2d4d5ec57624eb0b617f84979d6b810d82738f49
lolo <ldupire@diltd.com.vn>
2022-11-26 08:17:23 +0700

    prometheus.Handler is deprecated and replaced by promhttp.Handler instead

--- vendor/github.com/prometheus/client_golang/examples/simple/main.go
+++ vendor/github.com/prometheus/client_golang/examples/simple/main.go
@@ -18,13 +18,13 @@ import (
 	"flag"
 	"net/http"
 
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
 
 func main() {
 	flag.Parse()
-	http.Handle("/metrics", prometheus.Handler())
+	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(*addr, nil)
 }
